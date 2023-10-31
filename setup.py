b4da497d8682504eec07d132971bd8490b54207b
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2023-10-31 14:42:01 +0200

    add start structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="cli-bot",
+    description="Python-cli personal assistant",
+    url="https://github.com/icodebits/goitneo-python-project-1-team-15",
+    version="0.0.1",
+    license="MIT",
+    packages=find_namespace_packages(),
+    install_requires=["click"],
+)
