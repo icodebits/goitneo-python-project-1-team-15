2fc78c023098a31cd2886a57598b1eadb21afbca
OlliSensible <127432509+OlliSensible@users.noreply.github.com>
2023-11-02 17:06:04 +0100

    Correct

--- helpers/storage.py
+++ helpers/storage.py
@@ -3,7 +3,6 @@ import os, pickle
 from base.notes import Notes
 from base.address_book import AddressBook
 
-
 def load_data(filename):
     if os.path.exists(filename):
         with open(filename, "rb") as file:
