b1d9ec64b57c7c3d647e1552cad7c81eca5ee574
OlliSensible <127432509+OlliSensible@users.noreply.github.com>
2023-11-02 16:42:12 +0100

    new

--- helpers/storage.py
+++ helpers/storage.py
@@ -1,13 +1,15 @@
 import os, pickle
 
+from base.notes import Notes
+from email_address import AddressBook
+
 
 def load_data(filename):
     if os.path.exists(filename):
         with open(filename, "rb") as file:
             data = pickle.load(file)
         return data
-    return None
-
+    return {'contacts': AddressBook(), 'notes': Notes()}  
 
 def save_data(data, filename):
     with open(filename, "wb") as file:
